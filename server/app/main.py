f5d7dce15ff6f4816b1810d19f8b2eb24c9a33d8
simondh <simondharcourt@gmail.com>
2024-07-02 19:00:31 +0200

    Add groq and mistral

--- server/app/main.py
+++ server/app/main.py
@@ -8,6 +8,16 @@ from .config import DEBUG
 from .domain import Speech, PeopleList
 from .dependencies import datastore, executor, loop, chat
 
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+from .model.mistral import chat_mistral
+from .config import DEBUG, USE_GEMINI, USE_LANGCHAIN, USE_MISTRAL, USE_OPENAI, USE_GROQ
+from .classes import PeopleList, Speech, People
+from .model.gemini import chat_gemini
+from .model.langchain import chat_langchain
+from .model.openai import chat_gpt
+from .model.groq import chat_groq
+
 ##### API #################################
 origins = ["*"]
 app = FastAPI()
@@ -33,23 +43,67 @@ async def initialize(personList: PeopleList):
 
 @app.post("/hear")
 async def hear(speech: Speech):
-    if speech.noAnswerExpected:
-        datastore.hear(speech)
-        return
+    # if speech.noAnswerExpected:
+    #     datastore.hear(speech)
+    #     return
     
-    prompt = getPrompt(speech)
-    result = await loop.run_in_executor(executor, chat.chat, prompt)
+    # prompt = getPrompt(speech)
+    # result = await loop.run_in_executor(executor, chat.chat, prompt)
 
-    answer_speech = speech.answer_speech(result)
+    # answer_speech = speech.answer_speech(result)
 
-    datastore.converse(speech, answer_speech)
+    # datastore.converse(speech, answer_speech)
+    """
+    Parameters:
+    speech (Speech): An instance of the Speech class representing the speech heard by the NPC.
+    model (str): A string representing the NLP model to use for generating the response.
 
-# Return the NPC's name, the speaker's name, and the NPC's response
-    return {
-        "NPC": answer_speech.target.fullname(), 
-        "Speaker": answer_speech.speaker.fullname(), 
-        "Speech": f"{answer_speech.content}"
-    }
+    Returns:
+    dict: A dictionary containing the NPC's name, the speaker's name, and the NPC's response.
+
+    Raises:
+    None
+    """
+    # create a variable using to create files
+    var = speech.firstname+'_'+speech.lastname + '_' + speech.id + '.txt'
+
+    # Store the speech heard by the NPC in a file
+    with open("data/provisoire/heard_conversation_" + var, 'a', encoding='utf-8') as f:
+        f.write("\n"+speech.speaker + " ; " +
+                (speech.distance if speech.distance else '0') + ' ; ' + speech.content)
+
+    # Get the NPC's response to the speech if needed
+    if not speech.noAnswerExpected:
+        # Get the NPC's response to the speech using the specified NLP model
+        if USE_LANGCHAIN:
+            result = await loop.run_in_executor(executor, chat_langchain, speech)
+        elif USE_GEMINI:
+            result = await loop.run_in_executor(executor, chat_gemini, speech)
+        elif USE_MISTRAL:
+            result = await loop.run_in_executor(executor, chat_mistral, speech)
+        elif USE_OPENAI:
+            result = await loop.run_in_executor(executor, chat_gpt, speech)
+        elif USE_GROQ:
+            result = await loop.run_in_executor(executor, chat_groq, speech)
+            
+
+        # Store the NPC's response to the speech in a file 
+        with open("data/provisoire/conversations_" + var, 'a', encoding='utf-8') as f:
+            f.write("\n" + speech.speaker + ' : ' + speech.content)
+            f.write("\n" + speech.firstname + ' ' +
+                    speech.lastname + ':' + result)
+
+    # Return the NPC's name, the speaker's name, and the NPC's response
+        return {"NPC": speech.speaker, "Speaker": f"{speech.firstname} {speech.lastname}", "Speech": f"{result}"}
+    else:
+        return {"NPC": speech.speaker, "Speaker": f"{speech.firstname} {speech.lastname}", "Speech": ""}
+
+# # Return the NPC's name, the speaker's name, and the NPC's response
+#     return {
+#         "NPC": answer_speech.target.fullname(), 
+#         "Speaker": answer_speech.speaker.fullname(), 
+#         "Speech": f"{answer_speech.content}"
+#     }
         
 
 @app.get("/files/{file}")
---
+++ server/app/model/openai.py
@@ -0,0 +1,39 @@
+from ..utils import getPrompt
+from ..config import OPENAI_API_KEY, DEBUG, MODEL_NAME, OPENAI_API_KEY, USE_OPENAI
+from ..classes import Speech
+
+import openai
+
+if USE_OPENAI :
+    try:
+        openai.api_key = OPENAI_API_KEY
+    except Exception as e:
+        print(e)
+
+def chat_gpt(speech: Speech) -> str:
+    """
+    This function is used to generate a chat response using the GPT-3 model.
+
+    Parameters:
+    speech (Speech): An instance of the Speech class containing the input text.
+    model_name (str, optional): The name of the GPT-3 model to use. Defaults to 'text-davinci-002'.
+
+    Returns:
+    str: The generated chat response.
+
+    Note:
+    This function uses the OpenAI API to generate the chat response.
+    The input text is passed to the GPT-3 model, and the generated response is returned.
+    If the DEBUG constant is set to True, the chat response is printed to the console.
+    """
+
+    chat_response = openai.Completion.create(
+      engine=MODEL_NAME,
+      prompt=getPrompt(speech),
+      max_tokens=150
+    )
+
+    if DEBUG :
+        print(chat_response.choices[0].text.strip())
+
+    return chat_response.choices[0].text.strip()
